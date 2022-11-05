28897d18cd7341d2ea2476f796bdb8f720b33e2e
mahdiderbel <mohamed.mahdi.derbel@gmail.com>
2022-11-05 17:29:39 +0100

    first commit

---
+++ src/configuration.js
@@ -0,0 +1,17 @@
+export const Configuration = { 
+    API_BASEURL: 'http://127.0.0.1:5000/', 
+
+	FRONT_BASEURL: 'http://localhost:3000/',
+    BACK_BASEURL : 'http://localhost:4000/',
+
+	/* FRONT_BASEURL: 'http://54.38.180.6/',
+    BACK_BASEURL : 'http://54.38.180.6:8080/', */
+    
+    api_key : 'WU011uRaUe66Utp5YgaJANFMLiw0MPuZgf87djyX',
+    segments : 'http://37.187.225.45/rfm_opalia/',
+    clustering : 'http://37.187.225.45/clustering_opalia/',
+    mapping : 'http://37.187.225.45/mapping_opalia/',
+    transition : 'http://37.187.225.45/transition_opalia/',
+    highlight : 'http://37.187.225.45/highlight_opalia/',
+ };
+export default Configuration;
